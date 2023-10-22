67328248d581f97483fac24e1d7e1d56034a9cfc
Pixelzzz <sherzik99@gmail.com>
2023-10-23 00:38:07 +0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { ValidationPipe } from './pipes/validation.pipe';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   app.useGlobalPipes(new ValidationPipe());
   console.log(`Server running on http://localhost:${PORT}`);
   await app.listen(PORT);
